4919a336192bbe63a4844599f4b0be994b7d9cb8
Ranika Williams <rnkprice07@gmail.com>
2019-09-25 17:48:11 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_182023) do
+ActiveRecord::Schema.define(version: 2019_09_25_173037) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "artist_id"
   end
 
 end
